6cf7c4cef088d447df3104425d75d288cfb9a9d4
Pasan Mudalige <pasanmu@insharptechnologies.com>
2025-11-04 21:33:31 +0530

    step 1

--- app/controllers/dashboard.controller.js
+++ app/controllers/dashboard.controller.js
@@ -1,18 +1,56 @@
 const db = require("../models");
 const { httpResponseCode } = require("../constants/httpResponseCode");
-const config = require("../config/auth.config");
-const User = db.User;
-const TempUser = db.tempuser;
-const constants = require("../constants/constants");
-const { sendEmail } = require("../util/email.sender.util");
-const crypto = require("crypto");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
-const EmailTemplateDTO = require("../dto/EmailTemplateDTO");
 
 exports.getDashboardData = async (req, res) => {
-  return res.status(httpResponseCode.HTTP_RESPONSE_OK).send({
-    code: httpResponseCode.HTTP_RESPONSE_OK,
-    message: "getDashboard data under development",
-  });
+  try {
+    const { Student, Class, StudentClass, Sequelize } = db;
+
+    const [studentsCount, classesCount] = await Promise.all([
+      Student.count(),
+      Class.count(),
+    ]);
+
+    const recentStudents = await Student.findAll({
+      order: [["createdAt", "DESC"]],
+      limit: 5,
+      attributes: ["id", "student_name", "status", "createdAt"],
+    });
+
+    const recentClasses = await Class.findAll({
+      order: [["createdAt", "DESC"]],
+      limit: 5,
+      attributes: ["id", "class_name", "class_code", "status", "createdAt"],
+    });
+
+    const activeStudentsCount = await Student.count({ where: { status: "active" } });
+    const inactiveStudentsCount = studentsCount - activeStudentsCount;
+
+    const activeClassesCount = await Class.count({ where: { status: "active" } });
+    const inactiveClassesCount = classesCount - activeClassesCount;
+
+    return res.status(httpResponseCode.HTTP_RESPONSE_OK).send({
+      code: httpResponseCode.HTTP_RESPONSE_OK,
+      message: "Dashboard data loaded successfully",
+      data: {
+        students: {
+          total: studentsCount,
+          active: activeStudentsCount,
+          inactive: inactiveStudentsCount,
+          recent: recentStudents,
+        },
+        classes: {
+          total: classesCount,
+          active: activeClassesCount,
+          inactive: inactiveClassesCount,
+          recent: recentClasses,
+        },
+      },
+    });
+  } catch (error) {
+    return res.status(httpResponseCode.HTTP_RESPONSE_INTERNAL_SERVER_ERROR).send({
+      code: httpResponseCode.HTTP_RESPONSE_INTERNAL_SERVER_ERROR,
+      message: "Failed to load dashboard data",
+      error: error?.message || error,
+    });
+  }
 };
--- app/models/index.js
+++ app/models/index.js
@@ -24,11 +24,24 @@ db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
 db.User = require("./user.model.js")(sequelize, Sequelize);
+db.Student = require("./student.model.js")(sequelize, Sequelize);
+db.Class = require("./classes.model.js")(sequelize, Sequelize);
+db.StudentClass = require("./student-classes.model.js")(sequelize, Sequelize);
 
-// Associate models
-Object.keys(db).forEach((modelName) => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
+// Associations
+db.Student.belongsToMany(db.Class, {
+  through: db.StudentClass,
+  foreignKey: 'student_id',
+  otherKey: 'class_id'
 });
+db.Class.belongsToMany(db.Student, {
+  through: db.StudentClass,
+  foreignKey: 'class_id',
+  otherKey: 'student_id'
+});
+
+db.StudentClass.belongsTo(db.Student, { foreignKey: 'student_id' });
+db.StudentClass.belongsTo(db.Class, { foreignKey: 'class_id' });
+db.Student.hasMany(db.StudentClass, { foreignKey: 'student_id' });
+db.Class.hasMany(db.StudentClass, { foreignKey: 'class_id' });
 module.exports = db;
