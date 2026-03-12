fdbc34ea9b85e3118e7b357e230aca8ffd27df83
Pasan Mudalige <pasanmu@insharptechnologies.com>
2026-03-12 23:21:05 +0530

    migration files isue fixed

--- migrations/20260312180000-add-missing-columns-to-students.js
+++ migrations/20260312180000-add-missing-columns-to-students.js
@@ -2,10 +2,6 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn("students", "school", {
-      type: Sequelize.STRING(200),
-      allowNull: true,
-    });
 
     await queryInterface.addColumn("students", "year_of_al", {
       type: Sequelize.STRING(20),
@@ -23,6 +19,5 @@ module.exports = {
   async down(queryInterface, Sequelize) {
     await queryInterface.removeColumn("students", "hear_about_us");
     await queryInterface.removeColumn("students", "year_of_al");
-    await queryInterface.removeColumn("students", "school");
   },
 };
